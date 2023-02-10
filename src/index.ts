21b66cbd2e23b7e1758663b2bd47964311c590f4
Joaoof <joaodeus400@gmail.com>
2023-02-10 07:39:25 -0300

    models de categoria

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
-let texto: string;
+import express from 'express';
+import mongoose from 'mongoose';
 
-texto = '9289048309'
 
-console.log(texto)
+mongoose.connect('mongodb://localhost:27017')
+  .then(() => {
+    const app = express();
+    const port = 8070;
+
+    app.listen(port, () => {
+      console.log('Porta conectada!');
+    });
+  })
+  .catch(() => console.log('desconectado ao mongo'));
+
+
